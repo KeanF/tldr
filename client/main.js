2a6a4d44be2a5ac70f3baa2d4efc7c766a56e82c
KeanF <kf9zy@virginia.edu>
2016-07-06 23:00:41 -0400

    Add public and user side ui frames

--- client/main.js
+++ client/main.js
@@ -1,1 +1,14 @@
-import '../imports/ui/body.js'
+// Include approrpiate routing information to the UI
+import '../imports/public_ui/body.js'
+import '../imports/user_ui/body.js'
+
+/**
+ * Render {{> something}} if user logged in, {{> LOGIN}} if not
+ */
+function render (route, place) {
+    if (!Meteor.user()) {
+        route.render('login');
+    } else {
+        route.render(place);
+    }
+}
